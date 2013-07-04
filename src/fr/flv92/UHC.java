5698099e4163fec2e6eec06c76bb92f221bd7122
flv92 <florian.veaux@laposte.net>
2013-07-04 19:12:33 +0200

    Initial Commit

---
+++ src/fr/flv92/UHC.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.flv92;
+
+/**
+ *
+ * @author UndercastTeam <http://undercastteam.github.io>
+ */
+public class UHC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
